612cc731ed86d38cfcacd29353588b4cf92568b9
Carlos <carlosoc@live.com>
2023-08-06 15:19:28 -0600

    modifying router

--- src/router/router.js
+++ src/router/router.js
@@ -5,7 +5,7 @@ import MainComponent from '../components/MainComponent.vue'
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/planet/mercury' },
+    { path: '/', redirect: '/planet/Mercury' },
     { path: '/planet/:name', component: MainComponent },
   ]
 });
